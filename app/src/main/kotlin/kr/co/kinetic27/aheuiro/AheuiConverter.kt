5c956ad0ed2f8ff48b9623c0e03b5f1d5ddd0d73
Kinetic27 <ghdtjrwls27@gmail.com>
2019-10-14 19:23:17 +0900

    Add Admob Fix toast to SnackBar

--- app/src/main/kotlin/kr/co/kinetic27/aheuiro/AheuiConverter.kt
+++ app/src/main/kotlin/kr/co/kinetic27/aheuiro/AheuiConverter.kt
@@ -2,7 +2,6 @@ package kr.co.kinetic27.aheuiro
 
 import android.content.Context
 import org.json.JSONObject
-import java.lang.Exception
 import java.util.*
 
 /**
@@ -17,69 +16,61 @@ object AheuiConverter {
     private fun ClosedRange<Int>.random() =
             Random().nextInt(endInclusive - start) + start
 
-    fun loadData(context: Context) {
-        with(context.assets) {
-            open("numAheui.json").bufferedReader().use {
-                it.readText()
-            }.run {
-                numAheuiText = JSONObject(this)
+    private fun Context.loadJson(str: String) =
+            this.assets.open(str).bufferedReader().readText().run {
+                JSONObject(this)
             }
 
-            open("strAheui.json").bufferedReader().use {
-                it.readText()
-            }.run {
-                strAheuiText = JSONObject(this)
+    fun loadData(context: Context) = with(context) {
+        numAheuiText = loadJson("numAheui.json")
+        strAheuiText = loadJson("strAheui.json")
+    }
+
+    private fun numToAheui(str: String) = try {
+            with(numAheuiText?.getJSONArray(str)) {
+                this?.getString((0..this.length()).random()) ?: "err"
             }
+    } catch (e: Exception) {
+        str
+    }
+
+    private fun numAheuiDic(c: Char) = try {
+        strAheuiText?.getString(c.toString()) ?: "초기화 오류"
+    } catch (e: Exception) {
+        with(c.toInt().toString()) {
+            "빠".repeat(length - 1) + normalToAhuei(split(""))
         }
     }
 
-    private fun numToAheui(str: String) =
-            with(numAheuiText?.getJSONArray(str)) {
-                this?.getString((0..this.length()).random()) ?: "초기화 안됨"
-            }
-
-    private fun numAheuiDic(str: String) = try {
-        strAheuiText?.getString(str) ?: "초기화 안됨"
-    } catch (e: Exception) {
-        "err"
+    private fun normalToAhuei(asciiStr: List<String>) = when (asciiStr.size) {
+        1 -> asciiStr.toString()
+        else -> {
+            asciiStr[1] + asciiStr.joinToString("+*")
+                    .substring(4, (asciiStr.size - 1) * 3 - 2)
+                    .replace("0+*", "*")
+        }
     }
+
 
     fun toAheui(str: String): String {
         var isOverlap = false
         var aheuiText = "25*"
-        var convertedStr: String
-        var asciiStr: String
 
         (0 until str.length).forEach { charN ->
-            convertedStr = ""
 
             when (isOverlap) { //중복 체크
-                false -> { // 중복 X
-                    asciiStr = str[charN].toInt().toString()
-
-                    aheuiText += when {
-                        numAheuiDic(str[charN].toString()) != "err" -> numAheuiDic(str[charN].toString()) //사전 우선 검색
-
-                        else -> {
-                            convertedStr += "빠".repeat(asciiStr.length) + asciiStr[0] + "*"
-
-                            (0 until asciiStr.length - 2).forEach {
-                                if (asciiStr[it + 1].toString() != "0")
-                                    convertedStr += asciiStr[it + 1] + "+"
-
-                                convertedStr += "*"
-                            }
-
-                            convertedStr += asciiStr[asciiStr.length - 1] + "+"
-
-                            convertedStr
-                        }
+                true -> isOverlap = false  //중복 -> 한번 쉼
+                else -> {
+                    if((str[charN].toString().toIntOrNull() ?: 10) in (0..9)) {
+                        aheuiText += str[charN] + "망"
+                        return@forEach
+                    } else {
+                        aheuiText += numAheuiDic(str[charN])
                     }
                 }
-                else -> isOverlap = false //중복 -> 한번 쉼
             }
 
-            if (str.length > 1 && charN + 1 != str.length && str[charN] == str[charN + 1]) {
+            if (charN + 1 != str.length && str.length > 1 && str[charN] == str[charN + 1]) {
                 // 마지막이 아니거나 다음에 같은 문자가 올때 중복(빠) 처리
                 aheuiText += "빠"
                 isOverlap = true
@@ -88,13 +79,6 @@ object AheuiConverter {
             aheuiText += "맣"
         }
 
-        aheuiText += "희"
-
-        return aheuiText.split("").joinToString(separator = "") {
-            when {
-                it.isNotEmpty() -> numToAheui(it)
-                else -> ""
-            }
-        }
+        return aheuiText.split("").joinToString(separator = "") { numToAheui(it) } + "마희"
     }
 }
--- app/src/main/kotlin/kr/co/kinetic27/aheuiro/MainActivity.kt
+++ app/src/main/kotlin/kr/co/kinetic27/aheuiro/MainActivity.kt
@@ -1,40 +1,94 @@
 package kr.co.kinetic27.aheuiro
 
+import android.annotation.SuppressLint
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import com.google.android.material.snackbar.Snackbar
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.webkit.WebChromeClient
 import android.widget.EditText
-import android.widget.Toast
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import java.util.*
+
 
 class MainActivity : BaseActivity() {
+    private lateinit var mInterstitialAd: InterstitialAd
 
     override var viewId: Int = R.layout.activity_main
     override var toolbarId: Int? = R.id.toolbar
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate() {
+
+        MobileAds.initialize(this) {}
+        mInterstitialAd = InterstitialAd(this).apply {
+            adUnitId = "ca-app-pub-7380710508478373/3330847526"
+            loadAd(AdRequest.Builder().build())
+        }
+
         AheuiConverter.loadData(this)
+
+        with(webView) {
+            with(settings) {
+                javaScriptEnabled = true
+                javaScriptCanOpenWindowsAutomatically = true
+
+                //useWideViewPort = true
+                loadWithOverviewMode = true
+
+                setAppCacheEnabled(true)
+                setSupportZoom(true)
+                builtInZoomControls = true
+                displayZoomControls = false
+            }
+
+            webChromeClient = WebChromeClient()
+            loadUrl("file:///android_asset/aheui.html")
+        }
 
         change_mode.setOnClickListener {
             first_text.text = when(first_text.text) {
-                "아희로" -> "ㅁㄴㅇㄹ"
-                else -> "아희로"
+                "아희로" -> {
+                    aheuiRo.visibility = View.GONE
+                    anheuiRo.visibility = View.VISIBLE
+
+                    "않희로"
+                }
+                else -> {
+                    aheuiRo.visibility = View.VISIBLE
+                    anheuiRo.visibility = View.GONE
+
+                    "아희로"
+                }
             }
         }
 
         copy_btn.setOnClickListener {
-            (getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager).primaryClip = ClipData.newPlainText("아희로", result_keyboard.text)
-            Toast.makeText(this, "클립보드에 복사되었습니다!", Toast.LENGTH_SHORT).show()
+            (getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager).primaryClip =
+                    ClipData.newPlainText("아희로", result_keyboard.text)
+
+            Snackbar.make(main_layout, "클립보드에 복사되었습니다!", Snackbar.LENGTH_SHORT).apply {
+                setAction("확인") { dismiss() }
+                show()
+            }
+
+            //  if(Random().nextInt(10) > 4)
+                mInterstitialAd.show()
         }
 
         hint_keyboard.textChanged {
-            result_keyboard.text = when {
-                it.isNotEmpty() -> AheuiConverter.toAheui(it)
-                else -> "..."
-            }
+            if (first_text.text == "아희로")
+                result_keyboard.text = when {
+                    it.isNotEmpty() -> AheuiConverter.toAheui(it)
+                    else -> "..."
+                }
         }
     }
 
